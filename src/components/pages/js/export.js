77b5c7e733948bdec9c13ae87ef77066419f48e8
Prathamesh Dipak Nigade <prathameshdnigade@gmail.com>
2024-04-09 20:33:57 +0530

    chNGED

--- src/components/pages/js/export.js
+++ src/components/pages/js/export.js
@@ -3,12 +3,11 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShareAlt, faDownload} from '@fortawesome/free-solid-svg-icons';
-
-import "../css/share.css"
+import "../css/export-share.css"
 
 const ExportButton = ({ onClick }) => {
   return (
-    <button onClick={onClick} className="exportbutton" >
+    <button onClick={onClick} className="export-share-button exportbutton" >
     <FontAwesomeIcon icon={faDownload}/> Export 
   </button>
   );
