588ea71a14a33a3bf9ca43d004e44d05e0e41ec0
Adarsha Halder <pradiphalder70@gmail.com>
2024-04-19 10:40:47 +0530

    h

--- src/components/pages/js/home.js
+++ src/components/pages/js/home.js
@@ -1,7 +1,8 @@
 // Home.js
 import React from "react";
 import Navbar from "..//../shared/js/Home_login"; // Import your Navbar component
-import Hero from "../home/hero";
+// import Hero from "../home/hero.tsx";
+import { HeroParallaxDemo } from "../home/hero.tsx"; // Import HeroParallaxDemo instead of Hero
 import Feature from "../home/Feature";
 import TopFeature from "../home/TopFeature";
 import "../css/home.css";
@@ -12,10 +13,11 @@ function Home() {
     <div style={{ backgroundColor: "transparent", overflowX: 'hidden', overflowY: 'hidden' }}>
       {/* Render only the Navbar component on the Home page */}
       <Navbar />
-      <Hero />
-      <TopFeature />
+      {/* <Hero /> */}
+      <HeroParallaxDemo /> {/* Render the HeroParallaxDemo component instead of Hero */}
+      {/* <TopFeature />
       <Feature />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
