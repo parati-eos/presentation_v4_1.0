060e18003ea685ba7127f27d697530ba469eb947
Adarsha Halder <pradiphalder70@gmail.com>
2024-04-21 13:06:22 +0530

    as

--- src/components/shared/js/Home_login.js
+++ src/components/shared/js/Home_login.js
@@ -1,7 +1,8 @@
+// Navbar.js
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import ParatiLogo from "../../Asset/parati-logo.png";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom";
 import "../css/home_login.css";
 
 function Navbar() {
@@ -9,6 +10,21 @@ function Navbar() {
   const handleLogoClicked = () => {
     navigate("https://www.parati.in");
   };
+
+  const scrollToBlog = () => {
+    const blogSection = document.getElementById("blogs");
+    if (blogSection) {
+      blogSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const scrollToSamples = () => {
+    const samplesSection = document.getElementById("samples-section");
+    if (samplesSection) {
+      samplesSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <nav className="home-nav">
       <div className="home-navbar-container">
@@ -19,25 +35,24 @@ function Navbar() {
               alt="Parati Logo"
               className="home-navbar-logo"
               onClick={handleLogoClicked}
-            ></img>
+            />
           </Link>
         </div>
-        <div class="home-navbar-buttons">
-  <div class="dropdown-mobile">
-    <button class="home-navbar-section-button dropdown-btn">Menu</button>
-    <div class="dropdown-content">
-      <Link to="/auth/login" class="dropdown-link">Features</Link>
-      <Link to="/auth/login" class="dropdown-link">Samples</Link>
-      <Link to="/auth/login" class="dropdown-link">Blogs</Link>
-    </div>
-  </div>
-  <Link to="/auth/login" class="home-navbar-section-button">Features</Link>
-  <Link to="/auth/login" class="home-navbar-section-button">Samples</Link>
-  <Link to="/auth/login" class="home-navbar-section-button">Blogs</Link>
-  <Link to="/auth/login" class="home-navbar-button1">Log In</Link>
-  <Link to="/auth/login" class="home-navbar-button2">Get Started</Link>
-</div>
-
+        <div className="home-navbar-buttons">
+          <div className="dropdown-mobile">
+            <button className="home-navbar-section-button dropdown-btn">Menu</button>
+            <div className="dropdown-content">
+              <Link to="#" className="dropdown-link">Features</Link>
+              <button className="dropdown-link" onClick={scrollToSamples}>Samples</button>
+              <a href="#blogs" className="dropdown-link" onClick={scrollToBlog}>Blogs</a>
+            </div>
+          </div>
+          <Link to="/" className="home-navbar-section-button">Features</Link>
+          <a href="#samples-section" className="home-navbar-section-button" onClick={scrollToSamples}>Samples</a>
+          <a href="#blogs" className="home-navbar-section-button" onClick={scrollToBlog}>Blogs</a>
+          <Link to="/auth/login" className="home-navbar-button1">Log In</Link>
+          <Link to="/auth/login" className="home-navbar-button2">Get Started</Link>
+        </div>
       </div>
     </nav>
   );
