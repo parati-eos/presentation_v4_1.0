4a6691d43d74940951a0fe5acb56d89b5566ffbc
Adarsha Halder <pradiphalder70@gmail.com>
2024-05-08 11:15:59 +0530

    test

--- src/components/helper/googlepresentation-helper.js
+++ src/components/helper/googlepresentation-helper.js
@@ -23,7 +23,7 @@ const Googleslides = () => {
     const fetchSlidesData = async () => {
       try {
         await updateProgress();
-        const url = `http://localhost:5000/slides?userId=${userId}&formId=${formId}`;
+        const url = `https://zynth.ai/api/slides?userId=${userId}&formId=${formId}`;
         const response = await fetch(url);
         if (!response.ok) {
           throw new Error("Failed to fetch slides data");
