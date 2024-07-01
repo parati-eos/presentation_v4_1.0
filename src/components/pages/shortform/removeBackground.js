a30aff0f89112bb9b90a08019e429855ae1b21c5
Adarsha Halder <pradiphalder70@gmail.com>
2024-07-01 13:14:53 +0530

    build

--- src/components/pages/shortform/removeBackground.js
+++ src/components/pages/shortform/removeBackground.js
@@ -52,7 +52,7 @@ const removeBackground = async (file) => {
 
       const response = await axios.post('https://api.remove.bg/v1.0/removebg', formData, {
         headers: {
-          'X-Api-Key': 'pFxcEcmDM6C6xbzpSniFxYR6', // replace with your actual Remove.bg API key
+          'X-Api-Key': 'cbuqApeq8iosWBmj6K6wMfMd', // replace with your actual Remove.bg API key
         },
         responseType: 'blob',
       });
